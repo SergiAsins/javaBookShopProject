38476ad8f13c1ff611d142bfeed3750d7bbe6cbe
Selma Muley Salas <selma.muley@gmail.com>
2024-10-30 09:00:18 +0100

    feat: add class Book

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Book {
+    public String id;
+    public String titulo;
+    public String autor;
+
+    public void Book(id,titulo,autor){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+}
