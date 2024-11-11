b16f637d01afe462df8729d5fc4926c50740934e
juan ignacio <nho13@hotmail.com>
2024-11-11 16:18:51 +0100

    feat: add getall

--- src/main/java/org/example/ManageBooks/Main.java
+++ src/main/java/org/example/ManageBooks/Main.java
@@ -1,9 +1,7 @@
 package org.example.ManageBooks;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,18 +16,47 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connected");
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
 
         //createBook
-        try{
+        try {
             String sql = "INSERT INTO libros (title, author, isbn) VALUES ('Sapiens', 'Yuval Noah', 'A345')";
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement statement = connection.createStatement();
             statement.executeUpdate(sql);
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
+        // seleccionar
+        public List<Book> findALL() {
+
+            String sql = " SELECT * FROM books";
+
+            try {
+                Connection connection = DriverManager.getConnection(url, user, password);
+                Statement statement = connection.createStatement();
+
+                ResultSet res = statement.executeQuery(sql);
+
+                while (res.next()) {
+
+                    String isbn = res.getString("isbn");
+                    String title = res.getString("title");
+                    String author = res.getString("author");
+
+                    Book book = new Book(isbn, title, author);
+                }
+            }catch (SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+
+
+            }
+        }
+
+        // excoger
+
     }
-}
+
